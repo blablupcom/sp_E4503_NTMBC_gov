7e4b9b3acb0c854658ddce207958324f9a9e033c
blablupcom <o.volodin@yahoo.com>
2017-11-28 12:40:10 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -38,7 +38,7 @@ def validateFilename(filename):
 
 
 def validateURL(url):
-    try:
+    #try:
         r = requests.get(url.replace(" ", "%20"))
         count = 1
         while r.status_code == 500 and count < 4:
@@ -54,9 +54,9 @@ def validateURL(url):
         validURL = r.status_code == 200
         validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
         return validURL, validFiletype
-    except:
-        print ("Error validating URL.")
-        return False, False
+  #  except:
+  #      print ("Error validating URL.")
+  #      return False, False
 
 def validate(filename, file_url):
     validFilename = validateFilename(filename)
